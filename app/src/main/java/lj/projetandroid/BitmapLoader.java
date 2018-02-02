6514fba93e0dce660702e7a7be32722afb9e27ce
glartigue <ghislain.lartigue@etu.u-bordeaux.fr>
2018-02-02 15:53:10 +0100

    load from gallery (NYI)

--- app/src/main/java/lj/projetandroid/BitmapLoader.java
+++ app/src/main/java/lj/projetandroid/BitmapLoader.java
@@ -2,5 +2,9 @@ package lj.projetandroid;
 
 //Classe utilisée pour load & save les bitmaps
 
+import android.content.Intent;
+import android.view.View;
+
 public abstract class BitmapLoader {
+
 }
--- app/src/main/java/lj/projetandroid/MainActivity.java
+++ app/src/main/java/lj/projetandroid/MainActivity.java
@@ -1,6 +1,12 @@
 package lj.projetandroid;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,9 +18,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+   /* private static int RESULT_LOAD_IMG = 1;
+    public void loadImagefromGallery(View view) {
+        // Create intent to Open Image applications like Gallery, Google Photos
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }*/
+
+
+    private static final int SELECT_PICTURE_ACTIVITY_REQUEST_CODE = 0;
+
+    public void selectPicture(View v) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, SELECT_PICTURE_ACTIVITY_REQUEST_CODE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode) {
+            case SELECT_PICTURE_ACTIVITY_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    if (cursor.moveToFirst()) {
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        String filePath = cursor.getString(columnIndex);
+                        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                        ((ImageView)findViewById(R.id.imageView2)).setImageBitmap(bitmap);
+                    }
+                    cursor.close();
+                }
+                break;
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
